d24a57354a1d9d8dfda49381dc1c9cec9690ac7d
oliverpa-dev <oliver_pa@DESKTOP-KB60D0B.localdomain>
2021-05-12 14:41:13 +0200

    Created canvas, added color and shape

---
+++ app.js
@@ -0,0 +1,19 @@
+// variables for the canvas
+
+var canvas;
+var canvasContext;
+
+// when the pages loads
+window.onload = function() {
+    // get the html element with id of map
+    canvas = document.getElementById('map');
+    // context is for drawing graphics
+    canvasContext = canvas.getContext('2d');
+
+    // filling the canvas with black color
+    canvasContext.fillStyle = 'black';
+
+    // 0,0 is for x,y coordinates - top left corner
+    // canvas.width, canvas-height - bottom right corner
+    canvasContext.fillRect(0,0, canvas.width, canvas.height);
+}
