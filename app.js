a5a37504e8d9b6971b77af7f58aa19aad262b394
oliverpa-dev <oliver_pa@DESKTOP-KB60D0B.localdomain>
2021-05-12 16:34:35 +0200

    Added and learned how to deal with fillRect

--- app.js
+++ app.js
@@ -16,4 +16,10 @@ window.onload = function() {
     // 0,0 is for x,y coordinates - top left corner
     // canvas.width, canvas-height - bottom right corner
     canvasContext.fillRect(0,0, canvas.width, canvas.height);
+
+    canvasContext.fillStyle = 'red';
+    canvasContext.fillRect(300, 200, 400, 400);
+
+    canvasContext.fillStyle = "blue";
+    canvasContext.fillRect(250, 150, 100, 100);
 }
