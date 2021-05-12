0cc75057f3e12425f388a87376844d32212bcb60
oliverpa-dev <oliver.paszkiewicz@interia.pl>
2021-05-12 20:35:56 +0200

    added ball movement and speed

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-// variables for the canvas
+console.log("PongRemake Hurray!");
 
+// variables for canvas
 var canvas;
 var canvasContext;
 
-// defining our ball
+// defining our ball starting position leftX 
 var ballX = 50;
+
+// speed of the ball
+var ballSpeedX = 10;
 
 // when the pages loads
 window.onload = function() {
@@ -13,26 +17,40 @@ window.onload = function() {
     // context is for drawing graphics
     canvasContext = canvas.getContext('2d');
     // calling the function for drawing all the elements with interval of 1 seceond
+    // divided  by framesPerSecond
     var framesPerSecond = 30;
-    setInterval(drawAllElements, 1000/framesPerSecond);
+    setInterval(function() {
+        drawAllElements();
+        moveEverything();
+    }, 1000/framesPerSecond);
 
+}
+
+//function for moving the ball
+function moveEverything() {
+    ballX = ballX + ballSpeedX;
+    // if it exits the canvas width - flip the direction
+    if(ballX > canvas.width) {
+        ballSpeedX = -ballSpeedX;
+    }
+    // if it exits the canvas width - flip the direction
+    if(ballX < 0) {
+        ballSpeedX = -ballSpeedX
+    }
 }
 
 // function for drawing all the elements
 function drawAllElements() {
-
-    ballX = ballX + 5;
-
-    // filling the canvas with black color
-    canvasContext.fillStyle = 'black';
-
+    
     // 0,0 is for x,y coordinates - top left corner
     // canvas.width, canvas-height - bottom right corner
-    canvasContext.fillRect(0,0, canvas.width, canvas.height);
+    colorRect(0,0, canvas.width, canvas.height, 'black');
+    colorRect(0, 200, 10 , 80, 'white');
+    colorRect(ballX, 100, 10, 10, 'white');
+}
 
-    canvasContext.fillStyle = 'red';
-    canvasContext.fillRect(300, 200, 400, 400);
-
-    canvasContext.fillStyle = "blue";
-    canvasContext.fillRect(ballX, 100, 10, 10);
+// function that simplifies the code
+function colorRect(leftX, topY, width, height, drawColor) {
+    canvasContext.fillStyle = drawColor;
+    canvasContext.fillRect(leftX, topY, width, height);
 }
