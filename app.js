d80699edc2e2ceed7d43d62aa2bf8e24c2255a1a
oliverpa-dev <oliver.paszkiewicz@interia.pl>
2021-05-13 15:52:04 +0200

    Added ballY as well as function to move the paddle

--- app.js
+++ app.js
@@ -6,9 +6,30 @@ var canvasContext;
 
 // defining our ball starting position leftX 
 var ballX = 50;
+var ballY = 50;
 
 // speed of the ball
 var ballSpeedX = 10;
+var ballSpeedY = 4;
+
+// players paddle
+var paddle1Y = 250;
+const paddleHeight = 100;
+
+// function that caluclates the position of our mouse 
+function calculateMousePosition(evt) {
+    // this the area of black rectangle
+    var rect = canvas.getBoundingClientRect();
+    // this is the html document
+    var root = document.documentElement;
+    // checks where the rectangle is positioned in the browser
+    var mouseX = evt.clientX - rect.left - root.scrollLeft;
+    var mouseY = evt.clientY - rect.top - root.scrollTop;
+    return {
+        x:mouseX,
+        y:mouseY
+    }
+}
 
 // when the pages loads
 window.onload = function() {
@@ -24,30 +45,44 @@ window.onload = function() {
         moveEverything();
     }, 1000/framesPerSecond);
 
+    // adding event listener for mousemove
+    canvas.addEventListener('mousemove', function(evt) {
+        var mousePos = calculateMousePosition(evt);
+        // assignes the position of the paddle to the
+        // position of the mouse
+        paddle1Y = mousePos.y - paddleHeight/2;
+    })
+
 }
 
 //function for moving the ball
 function moveEverything() {
     ballX = ballX + ballSpeedX;
+    ballY = ballY + ballSpeedY;
     // if it exits the canvas width - flip the direction
     if(ballX > canvas.width) {
         ballSpeedX = -ballSpeedX;
     }
+    if(ballY > canvas.height) {
+        ballSpeedY = -ballSpeedY;
+    }
     // if it exits the canvas width - flip the direction
     if(ballX < 0) {
-        ballSpeedX = -ballSpeedX
+        ballSpeedX = -ballSpeedX;
+    }
+    if(ballY < 0) {
+        ballSpeedY = -ballSpeedY;
     }
 }
 
 // function for drawing all the elements
 function drawAllElements() {
-    
     // 0,0 is for x,y coordinates - top left corner
     // canvas.width, canvas-height - bottom right corner
     colorRect(0,0, canvas.width, canvas.height, 'black');
-    colorRect(0, 200, 10 , 80, 'white');
+    colorRect(0, paddle1Y, 10 , paddleHeight, 'white');
     // drawing the ball
-    colorCircle(ballX, 150, 10, 'white');
+    colorCircle(ballX, ballY, 10, 'white');
 }
 
 // function that simplifies the code for rect
@@ -63,3 +98,4 @@ function colorCircle(centerX, centerY, radius, drawColor) {
     canvasContext.arc(centerX, centerY, radius, 0, Math.PI*2, true);
     canvasContext.fill();
 }
+
